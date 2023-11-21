a01d52bbae82877d41b095d980fc273e406bd479
Abdullah Amin <abdullahamin231@gmail.com>
2023-11-21 18:55:23 +0500

    started

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function getBook(){
     el = document.getElementById("modal");
-el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+    document.body.classList.add('modal-opened')
 }
